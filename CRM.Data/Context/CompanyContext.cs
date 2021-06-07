0b28db5c4587c4b742b80d9f19008c96e87b0a8e
Doğan Aker <dogan234@gmail.com>
2021-06-07 17:06:11 +0300

    Login Controller bug fix

--- CRM.Data/Context/CompanyContext.cs
+++ CRM.Data/Context/CompanyContext.cs
@@ -15,8 +15,8 @@ namespace CRM.Data.Context
             optionsBuilder.UseNpgsql(@"Server=ec2-63-34-97-163.eu-west-1.compute.amazonaws.com;Database=daiunk0tl6larq;UID=ecmgnxpbwbsmyi;PWD=29a54c4cd0d29a6e32285a20d4719436a50d893bfee3929c85b7ebb05b462d2c;SSL Mode= Require;TrustServerCertificate=True");
         }
 
-        DbSet<Employee> Employees { get; set; }
-        DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
 
     }
 }
--- CRM.UI/Controllers/LoginController.cs
+++ CRM.UI/Controllers/LoginController.cs
@@ -28,57 +28,57 @@ namespace CRM.UI.Controllers
         }
 
 
-        //[HttpPost]
-        //public async Task<IActionResult> Index(LoginDto model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        //var employee = _context.Employees.FirstOrDefault(x => x.FullName == model.Username && x.Password == model.Password);
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = _context.Employees.FirstOrDefault(x => x.FullName == model.Username && x.Password == model.Password);
 
-        //        //var employee1 = _context.Employees.FirstOrDefault(x => x.FullName == model.Username && x.Password == model.Password);
-
-                
-        //        if (employee != null)
-        //        {
-        //            var claims = new List<Claim>
-        //        {
-        //            new Claim(ClaimTypes.Sid,employee.ID.ToString()),
-        //            new Claim(ClaimTypes.Name,employee.FullName),
-        //            new Claim(ClaimTypes.UserData,employee.Role)
-        //        };
-        //            var userIdentity = new ClaimsIdentity(claims, "login");
-        //            ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-        //            await HttpContext.SignInAsync(principal);
-
-        //            _context.SaveChanges();
-
-        //            if (HttpContext.User.Identity.IsAuthenticated)
-        //            {
-        //                if (HttpContext.User.Claims.ToArray()[2].Value != null)
-        //                {
+                //var employee1 = _context.Employees.FirstOrDefault(x => x.FullName == model.Username && x.Password == model.Password);
 
 
-        //                    TempData["UserID"] = HttpContext.User.Claims.ToArray()[0].Value;
-        //                    TempData["UserName"] = HttpContext.User.Claims.ToArray()[1].Value;
-        //                    TempData["UserRole"] = HttpContext.User.Claims.ToArray()[2].Value;
-        //                }
+                if (employee != null)
+                {
+                    var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Sid, employee.Id.ToString()),
+                    new Claim(ClaimTypes.Name,employee.FullName),
+                    new Claim(ClaimTypes.UserData,employee.Role)
+                };
+                    var userIdentity = new ClaimsIdentity(claims, "login");
+                    ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                    await HttpContext.SignInAsync(principal);
 
-        //            }
+                    _context.SaveChanges();
 
-        //            return RedirectToAction("Index", "Home");
-        //        }
-        //        else
-        //        {
-        //            ViewBag.error = "E-Mail or Password wrong!";
-        //            return View();
-        //        }
+                    if (HttpContext.User.Identity.IsAuthenticated)
+                    {
+                        if (HttpContext.User.Claims.ToArray()[2].Value != null)
+                        {
 
-        //    }
-        //    else
-        //    {
-        //        return View();
-        //    }
 
-        //}
+                            TempData["UserID"] = HttpContext.User.Claims.ToArray()[0].Value;
+                            TempData["UserName"] = HttpContext.User.Claims.ToArray()[1].Value;
+                            TempData["UserRole"] = HttpContext.User.Claims.ToArray()[2].Value;
+                        }
+
+                    }
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ViewBag.error = "E-Mail or Password wrong!";
+                    return View();
+                }
+
+            }
+            else
+            {
+                return View();
+            }
+
+        }
     }
 }
